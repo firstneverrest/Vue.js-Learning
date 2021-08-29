11197c0610f3c6a3eb390f5795113128c9e70da9
Chitsanupong <first30952@gmail.com>
2021-08-29 11:42:08 +0700

    feat: add conditional rendering

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      showBooks: true,
       title: 'The Great Empire',
       author: 'Chitsanupong',
       age: 21,
@@ -9,6 +10,9 @@ const app = Vue.createApp({
   methods: {
     changeTitle(title) {
       this.title = title;
+    },
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
     },
   },
 });
