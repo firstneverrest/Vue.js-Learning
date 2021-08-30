e4335cd2324ed8f6f9ca4985c2ca570a85491313
Chitsanupong <first30952@gmail.com>
2021-08-30 21:37:17 +0700

    feat: add emit custom events and modal

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name" />
+  <button @click="handleClick">Change Title</button>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" @close="toggleModal" />
+  </div>
+  <button @click="toggleModal">open modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Modal,
+  },
+  data() {
+    return {
+      title: 'My first Vue App',
+      header: 'This is Modal',
+      text: 'Welcome to Vue project',
+      showModal: false,
+    };
+  },
+  methods: {
+    handleClick() {
+      const title = this.$refs.name.value;
+      this.title = title;
+      this.$refs.name.classList.add('active');
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.active {
+  border: 2px solid #fcba03;
 }
 </style>
---
+++ vue-cli/src/components/Modal.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="backdrop" @click="closeModal">
+    <div class="modal">
+      <h3>{{ header }}</h3>
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  props: ['header', 'text'],
+  methods: {
+    closeModal() {
+      this.$emit('close');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: #fff;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+</style>
